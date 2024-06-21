ec647d13e4efb16b478cc23d5cf2726babc87877
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2024-06-20 21:26:10 -0400

    lets hope for the best

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -68,7 +68,7 @@ function Home(props) {
       <p>&nbsp;</p>
       <div className='about' >
           <h1 style={{fontFamily:'face' , color:'white'}}>About</h1>
-          <p>I have this website as my capstone assignment for Per Scholas, and as way of showcasing my skills to future employers, people interested in my work, my family and friends, and to myself. In the end my reason for making this assignment to fullest potential is because of this key moral, my work isn't just for those who asked for it, but also to <a style={{color:'lightBlue'}}href='https://biblehub.com/bsb/colossians/3.htm#:~:text=23Whatever%20you%20do%2C%20work%20at%20it%20with%20your%20whole%20being%2C%20for%20the%20Lord%20and%20not%20for%20men%2C%2024because%20you%20know%20that%20you%20will%20receive%20an%20inheritance%20from%20the%20Lord%20as%20your%20reward.%20It%20is%20the%20Lord%20Christ%20you%20are%20serving.'>present my best work to God</a> </p>
+          <p>I have this website as my capstone assignment for Per Scholas, and as way of showcasing my skills to future employers, people interested in my work, my family and friends, and to myself. In the end my reason for making this assignment to fullest potential is because of this key moral, my work isn't just for those who asked for it, but also to <a style={{color:'lightBlue'}}href='https://biblehub.com/bsb/colossians/3.htm#:~:text=23Whatever%20you%20do%2C%20work%20at%20it%20with%20your%20whole%20being%2C%20for%20the%20Lord%20and%20not%20for%20men%2C%2024because%20you%20know%20that%20you%20will%20receive%20an%20inheritance%20from%20the%20Lord%20as%20your%20reward.%20It%20is%20the%20Lord%20Christ%20you%20are%20serving.' target='blank'>present my best work to God</a> </p>
         </div>
         <p>&nbsp;</p>
     </div>
